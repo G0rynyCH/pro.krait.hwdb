b30fe994b6f7897a1654ba2d882fabdc9b83f5f3
root <root@localhost.localdomain>
2016-02-21 18:18:58 +0200

    Test Commit

---
+++ src/main/java/pro/krait/hwdb/entity/User.java
@@ -0,0 +1,107 @@
+package pro.krait.hwdb.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+/**
+ * Created by gorynych on 21.02.16.
+ */
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(name = "increment", strategy = "increment")
+    private long id;
+
+    @Column(name = "surname", nullable = false, length = 50)
+    private String surname;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "fatherName", length = 50)
+    private String fatherName;
+
+    @Column(name = "login", nullable = false, length = 20)
+    private String login;
+
+    @Column(name = "password", nullable = false, length = 50)
+    private String passwd;
+
+    @Column(name = "phoneNumber", length = 20)
+    private String phoneNumber;
+
+    @Column(name = "email", length = 30)
+    private String email;
+
+    public User() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFatherName() {
+        return fatherName;
+    }
+
+    public void setFatherName(String fatherName) {
+        this.fatherName = fatherName;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
