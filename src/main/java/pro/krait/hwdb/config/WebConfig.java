716bd42e7615ef76b516cd38b34a21c28b1dd11b
Gorin Sergey <S.S.Gorin@gmail.com>
2016-02-23 15:43:56 +0200

    Lesson 6 finished

--- src/main/java/pro/krait/hwdb/config/WebConfig.java
+++ src/main/java/pro/krait/hwdb/config/WebConfig.java
@@ -1,8 +1,16 @@
 package pro.krait.hwdb.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by gss on 19.02.2016.
@@ -11,5 +19,15 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Configuration                                      //Указывает, что данный класс является конфигурацией, которую нужно выполнить перед разворачиванием контекста Spring
 @EnableWebMvc                                       //Включение режима WebMVC - возможность использования контроллеров и Web-контроллеров
 @ComponentScan("pro.krait.hwdb")                    //Указывает, где искать наши бины
-public class WebConfig {
+public class WebConfig extends WebMvcConfigurerAdapter{
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(new ObjectMapper());
+        converter.setSupportedMediaTypes(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+        converters.add(converter);
+    }
 }
--- src/main/java/pro/krait/hwdb/controller/UserController.java
+++ src/main/java/pro/krait/hwdb/controller/UserController.java
@@ -1,22 +1,40 @@
 package pro.krait.hwdb.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import pro.krait.hwdb.entity.User;
 
 /**
  * Created by gss on 19.02.2016.
  * User Servlet
  */
-@Controller
-@RequestMapping("/users")
+/*
+ * Аннотация говорит, что данный класс является контроллером или сервлетом по другой терминологии
+ * Так же аннотация сообщает, что все методы данного контроллера будут возвращать JSON объекты
+*/
+@RestController
+@RequestMapping("/users")               //Аннотация говорит, что контроллер будет доступен по указанному адресу
 public class UserController {
 
     @RequestMapping(value = "/getuser", method = RequestMethod.GET)
     @ResponseBody                                                   //Указываем, что хотим вернуть именно строку.
-    public String getUser(ModelMap model){
-        return ("USER");
+    public User getUser(){
+        return createNewUser();
+    }
+
+    private User createNewUser() {
+        User user = new User();
+        user.setId(1);
+        user.setSurname("Gorin");
+        user.setName("Sergey");
+        user.setFatherName("Stanislavovich");
+        user.setLogin("agss");
+        user.setPasswd("12345");
+        user.setPhoneNumber("5552222");
+        user.setEmail("admin@skywell.com.ua");
+        return user;
     }
 }
